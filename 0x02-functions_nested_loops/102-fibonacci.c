d91766496368a842bf483e39e8e99d6c42659ceb
Naga0514 <ekhatornaga@gmail.com>
2022-09-14 09:56:58 -0700

    Fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,9 +3,9 @@
 #include <math.h>
 
 /**
- *  main - main block
- *  Description: Print the sum of all multiples of 3 or 5 below 1024.
- *  Return: 0
+ * main - main block
+ * Description: Print the sum of all multiples of 3 or 5 below 1024.
+ * Return: 0
  */
 int main(void)
 {
