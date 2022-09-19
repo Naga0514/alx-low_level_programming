266567b9285aafa52aed412dd581085e2c61d33d
Naga0514 <ekhatornaga@gmail.com>
2022-09-19 13:01:39 -0700

    hot fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,16 @@
 #include "main.h"
-
+#include <string.h>
 /**
- * puts2 - prints characters ;
+ * puts2 -> puts2 function
  * @str: string characters
  */
 
 void puts2(char *str)
 {
-	int index = 0, len = 0;
+	int len, i;
 
-	while (str[index++])
-		len++;
-	for (index = 0; index < len; index += 2)
-		_putchar(str[index]);
-
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
 	_putchar('\n');
 }
