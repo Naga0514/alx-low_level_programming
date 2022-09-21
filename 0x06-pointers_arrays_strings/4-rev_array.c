c126d93396f23b5de833d0746116eca86c836913
Naga0514 <98357891+Naga0514@users.noreply.github.com>
2022-09-21 22:29:05 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,26 +1,20 @@
 #include "main.h"
 
 /**
- * reverse_array - Write a function that reverses the content of an array of
- * integers.
- *
- * @a: This is the input array
- * @n: This is the positions have the array
- *
+ * reverse_array - reverse the contents of an array of integers
+ * @a: array of integers
+ * @n: number of elements in array
  */
+
 void reverse_array(int *a, int n)
 {
-	int i, last;
+	int i;
+	int h;
 
-	last = n - 1;
-	for (i = 0; i < n / 2; i++)
+	for (i = 0; i < (n / 2); i++)
 	{
-		int start, end;
-
-		start = a[i];
-		end = a[last];
-		a[i] = end;
-		a[last] = start;
-		last--;
+		h = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = h;
 	}
 }
