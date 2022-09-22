3bbe4d06bc2f2e0c55d7d16e753f0202deaf8cd0
Naga0514 <98357891+Naga0514@users.noreply.github.com>
2022-09-22 11:29:08 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,35 +1,23 @@
 #include "main.h"
 /**
  *_strncat- append string
- *@dest: checked
- *@src: checked
- *@n: checked
- *Return: dest
+ *@dest: concentrate two strings but add inputted number of bytes
+ *@src: string to be appended upon
+ *@n: integer parameter to compare index to
+ *Return: returns new concatenated string
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	
+	int index = 0, dest_len = 0;
 
-	i = 0;
+	while (dest[index++])
+		dest_len++;
+	
 
-	j = 0;
-
-	while (*(dest + i) != '\0')
-	{
-		i++;
-		j++;
-	}
-
-	i = 0;
-
-	while (*(src + i) != *(src + n))
-	{
-		dest[j] = src[i];
-		i++;
-		j++;
-	}
-	dest[j] = '\0';
-
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	
 	return (dest);
 }
