97fbd8d86ea02c040f96955987a9c5a0325b339b
Naga0514 <ekhatornaga@gmail.com>
2022-09-26 14:12:28 -0700

    task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,26 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * print_diagsums - function that prints the sum of the two diagonals
- *@a: first value -char
- *@size: second value -int
- */
-void print_diagsums(int *a, int size)
-{
-	int i, j, suma_1 = 0, suma_2 = 0, k, l = 0;
 
-	k = size - 1;
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-				suma_1 += a[l];
-			if (j == k)
-				suma_2 += a[l];
-			l++;
-		}
-		k--;
-	}
-	printf("%i, %i\n", suma_1, suma_2);
+/**
+ * set_string - sets the value of a pointer to a char
+ *
+ * @s: Double pointer
+ *
+ * @to: Pointer
+ *
+ * Return: Void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
 }
