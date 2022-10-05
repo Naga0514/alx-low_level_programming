ebadad53da84d1aaf356b34fde02d3b6568bf1a2
Naga0514 <ekhatornaga@gmail.com>
2022-10-05 11:36:58 -0700

    Done

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
-* free_grid -> removing memory allocation
-* @grid: grid to be freed
-* @height: height of matrix
-* Return: nothing
-*/
+ * free_grid - removing memory allocation
+ * @grid: grid to be freed
+ * @height: height of matrix
+ * Return: nothing
+ */
 void free_grid(int **grid, int height)
 {
         int i;
