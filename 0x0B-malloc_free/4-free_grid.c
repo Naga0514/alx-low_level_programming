12e52da8960f5525ad8e4f2dad619d8e5de0197f
Naga0514 <98357891+Naga0514@users.noreply.github.com>
2022-10-05 09:25:00 +0100

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,1 +1,18 @@
-FHGHH
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* free_grid - free memory dynamically allocated
+* @grid: pointer to 2-D array
+* @row: row of the array
+*/
+
+void free_grid(int **grid, int row)
+{
+	int i;
+
+	for (i = 0; i < row; i++)
+		free(grid[i]);
+	free(grid);
+}
