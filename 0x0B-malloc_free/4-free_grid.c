96eb7fc18e68a5059a3185345fb47a4b6d91e445
Naga0514 <98357891+Naga0514@users.noreply.github.com>
2022-10-05 18:45:32 +0100

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,19 +1,39 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * free_grid - frees a 2 dimensional grid.
- * @grid: multidimensional array of integers.
- * @height: height of the grid.
- *
- * Return: no return
- */
-void free_grid(int **grid, int height)
+* alloc_grid -> allocatingn 2d array
+* @width: width of an array
+* @height: height of an array
+* Return: a pointer to a allocated grid
+*/
+int **alloc_grid(int width, int height)
 {
-	if (grid != NULL && height != 0)
-	{
-		for (--height, height >= 0; --height)
-			free(grid[height]);
-		free(grid);
-	}
+        int i, j, k, l;
+        int **a;
+        if (width <= 0 || height <= 0)
+                return (NULL);
+        a = malloc(height * sizeof(int *));
+        if (a == NULL)
+        {
+                free(a);
+                return (NULL);
+        }
+        for (i = 0; i < height; i++)
+        {
+                a[i] = malloc(width * sizeof(int));
+                if (a[i] == NULL)
+                {
+                        for (j = i; j >= 0; j--)
+                        {
+                                free(a[j]);
+                        }
+                        free(a);
+                        return (NULL);
+                }
+        }
+        for (k = 0; k < height; k++)
+        {
+                for (l = 0; l < width; l++)
+                        a[k][l] = 0;
+        }
+        return (a);
 }
