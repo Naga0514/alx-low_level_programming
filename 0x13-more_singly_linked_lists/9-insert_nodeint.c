03dba80194fb5b48889bf583922b0b2bffbeb1b2
Naga0514 <98357891+Naga0514@users.noreply.github.com>
2022-10-26 14:58:56 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,49 +1,38 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - This function inserts a
- * new node at given position
- * @head: The head node
- * @idx: The index of the list where new node is to be added
- * @n: The integer data of the new node
- * Return: The address of the new inserted node or NULL if failed
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node, *temp = *head;
-	size_t i = 0;
+	listint_t *node, *tmp = *head, *half;
+	unsigned int i;
 
 	node = malloc(sizeof(listint_t));
-	if (!node)
+	if (node == NULL)
 		return (NULL);
-
 	node->n = n;
-	node->next = NULL;
-
-	if (!*head && !idx)
+	if (idx == 0)
 	{
+		node->next = *head;
 		*head = node;
 		return (node);
 	}
-
-	else if (!*head && idx)
-		return (NULL);
-
-	else if (!idx)
+	for (i = 0; i + 1 < idx; i++)
 	{
-		node->next = temp;
-		*head = node;
-		return (node);
+		if (tmp->next == NULL)
+		{
+			return (NULL);
+		}
+		tmp = tmp->next;
 	}
-
-	while (i < (idx - 1))
-	{
-		temp = temp->next;
-		i++;
-	}
-	node->next = temp->next;
-	temp->next = node;
-
+	half = tmp->next;
+	tmp->next = node;
+	node->next = half;
 	return (node);
 }
-Footer
-© 2022 GitHub, Inc.
